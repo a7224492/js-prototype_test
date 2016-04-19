dd0979fbf683d3c0fdeb4855d5e468722537750d
a7224492 <314158258@qq.com>
2016-04-19 21:06:00 +0800

    none

---
+++ ball/config.lua
@@ -0,0 +1,15 @@
+--Ball params
+BALL_START_SPEED=0.3
+BALL_INIT_Y=350
+BALL_INIT_X=160
+
+--obstacle params
+OBSTALCE_NUM=2 
+
+--enemy params
+ENEMY_SPEED=1.0
+
+--player params
+PLAYER_SPEED=0.1
+
+music="invoke.ogg"
